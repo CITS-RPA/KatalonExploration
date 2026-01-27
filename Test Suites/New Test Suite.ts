7382389f0646f506404d2dcbe29c911db091f342
weixiong.seah <weixiong.seah@269-92XDR14.staff.main.ntu.edu.sg>
2026-01-27 13:26:58 +0800

    Commit after completing Test Suite

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>fc618187-bf22-4a09-8b7b-96f839da4aae</testSuiteGuid>
+   <testCaseLink>
+      <guid>76c4def3-3f6a-4650-a4b6-b017eb9a438b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/New Test Case</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>98616e2c-b773-4713-9ff2-8c2d10240ff9</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/New Test Data</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>98616e2c-b773-4713-9ff2-8c2d10240ff9</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>NRIC</value>
+         <variableId>819e14ce-5a9c-492b-bfa3-70575d4e9e2a</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>98616e2c-b773-4713-9ff2-8c2d10240ff9</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>Name</value>
+         <variableId>ed6c331a-b0d5-4aa5-9c26-1a7507766e80</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
